a63c7732302ca21b572934c795f94da8befa3f71
Salwa <salwa@Salwas-Mac.local>
2018-05-01 04:21:36 +0200

    factorial

--- FactorialAssignment/ViewController.swift
+++ FactorialAssignment/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    @IBAction func getFactorialButton(_ sender: Any) {
+        
+        
+        getFactorial()    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // how to restrict numberTextField to accept only numbers??*********
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  
+    /*
+     * function to validate user's input and get factorial
+     */
+    
+  func getFactorial(){
+        
+          if (numberTextField.text?.isEmpty)!{
+            
+            // display alert msg when text field is empty
+            
+                alertLabel.text = "please Enter number"
+            
+          }else{
+            
+            alertLabel.text = "" // delete previous alert msg
+            
+            let number =  Int(numberTextField.text!) //number that user entered
+            var fact :Int = 1
+            let n  : Int = number! + 1
+            
+            for i in 1..<n {
+                
+                fact = fact * i
+                resultLabel.text = String (fact) // casting the result and display in label
+                
+            }
+            
+            
+            }
+        
+            }
+    
 }
 
